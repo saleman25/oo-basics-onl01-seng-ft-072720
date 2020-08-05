fc684cf3145361ab014fbd7cb2bfbd6f43ac5f58
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-05 14:52:41 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,14 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
  
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+  
  
 end 
 
-end 
+ 
